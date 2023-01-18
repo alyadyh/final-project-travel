cd00b6d218e127c25a69936e64ef1ad25fb21720
alyadyh <alya.mardhiyyah@binus.ac.id>
2023-01-18 11:08:00 +0700

    update package.java

--- src/Packages.java
+++ src/Packages.java
@@ -23,13 +23,33 @@ class Packages{
         this.price = harga;
         this.jumlahtiket = jumlah;
     }
+
+    void set_namapaket(String pakett){
+        this.namapaket = pakett;
+    }
+
+    void set_jenisken(String jenisken){
+        this.jeniskendaraan = jenisken;
+    }
+
+    void set_dest(String dest){
+        this.destinasi = dest;
+    }
+
+    void set_price(int harga){
+        this.price = harga;
+    }
+
+    void set_jumlahtiket(int jumlah){
+        this.jumlahtiket = jumlah;
+    }
     
     void get_data(){
-        System.out.println("Package Name: "+this.namapaket);
-        System.out.println("Transportation: "+this.jeniskendaraan);
-        System.out.println("Destination: "+this.destinasi);
-        System.out.println("Price: "+this.price);
-        System.out.println("Ticket Amount: "+this.jumlahtiket);
+        System.out.println("Package Name: " + this.namapaket);
+        System.out.println("Transportation: " + this.jeniskendaraan);
+        System.out.println("Destination: " + this.destinasi);
+        System.out.println("Price: " + this.price);
+        System.out.println("Ticket Amount: " + this.jumlahtiket);
     }
     void print_data(){
             System.out.println(this.namapaket + "\t || " + this.jeniskendaraan +  "\t || " +
