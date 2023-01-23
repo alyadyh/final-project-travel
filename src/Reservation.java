ea1c957e8b08f2bdd002e65998000377fc452439
alyadyh <alya.mardhiyyah@binus.ac.id>
2023-01-23 15:11:43 +0700

    delete comment

--- src/Reservation.java
+++ src/Reservation.java
@@ -15,7 +15,7 @@ class Reservation extends Account{
         this.increment = incr;
     }
     
-     void get_data(){
+    void get_data(){
         String t = "";
         String subchar = super.username.substring(0,4);
         for (int i = 0; i<subchar.length();++i) {
@@ -29,17 +29,3 @@ class Reservation extends Account{
         System.out.println("ResID" + t + this.increment);
     }
 }
-
-    // Reservation(String id,String pass,int incr){
-    //     super(id,pass);
-    // }
-    
-    // void set_data(String id,String pass){
-    //     super.username = id;
-    //     super.password = pass;
-    // }
-    
-    // protected <T> void get_data(T apaaja){
-    //     System.out.println("Reservation: ");
-    //     System.out.println("ResID" + Integer.parseInt(super.username) + apaaja);
-    // }
